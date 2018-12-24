3308bc4c288ae7df00798454dd7f283d3172790f
Jason Ngo <jason.n.ngo@gmail.com>
2018-12-24 18:42:18 -0500

    Added current image selection indicators on user details screen

--- Tinder/UserDetails/SwipingPhotoController.swift
+++ Tinder/UserDetails/SwipingPhotoController.swift
@@ -20,20 +20,47 @@ class SwipingPhotoController: UIPageViewController {
       })
       
       setViewControllers([controllers.first!], direction: .forward, animated: false, completion: nil)
+      setupBarViews()
     }
   }
   
+  let barsStackView = UIStackView(arrangedSubviews: [])
   var controllers: [UIViewController] = []
   
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
     dataSource = self
+    delegate = self
+  }
+  
+  fileprivate func setupBarViews() {
+    imageUrls?.forEach { _ in
+      let barView = UIView()
+      barView.backgroundColor = UIColor(white: 0, alpha: 0.1)
+      barView.layer.cornerRadius = 2
+      barsStackView.addArrangedSubview(barView)
+    }
+    
+    barsStackView.arrangedSubviews.first?.backgroundColor = .white
+    barsStackView.spacing = 4
+    barsStackView.distribution = .fillEqually
+    
+    view.addSubview(barsStackView)
+    let paddingTop = UIApplication.shared.statusBarFrame.height + 8
+    barsStackView.anchor(
+      top: view.topAnchor,
+      leading: view.leadingAnchor,
+      bottom: nil,
+      trailing: view.trailingAnchor,
+      padding: .init(top: paddingTop, left: 8, bottom: 0, right: 8),
+      size: .init(width: 0, height: 4)
+    )
   }
   
 }
 
-extension SwipingPhotoController: UIPageViewControllerDataSource {
+extension SwipingPhotoController: UIPageViewControllerDataSource & UIPageViewControllerDelegate {
   
   func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
     let index = controllers.firstIndex { $0 == viewController } ?? 0
@@ -45,6 +72,14 @@ extension SwipingPhotoController: UIPageViewControllerDataSource {
     let index = controllers.firstIndex { $0 == viewController } ?? 0
     if index == controllers.count - 1 { return nil }
     return controllers[index + 1]
+  }
+  
+  func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+    let currentViewController = viewControllers?.first
+    if let index = controllers.firstIndex(where: { $0 == currentViewController }) {
+      barsStackView.arrangedSubviews.forEach { $0.backgroundColor = UIColor(white: 0, alpha: 0.1) }
+      barsStackView.arrangedSubviews[index].backgroundColor = .white
+    }
   }
   
 }
